c9d4157fbe0c7f1db5ffbce1f65f0dabbfa6f95b
Adam15 <sochoradam15@gmail.com>
2025-05-01 11:44:53 +0200

    Přidána vesnice, vylepšen úvod

--- RPGHra/Program.cs
+++ RPGHra/Program.cs
@@ -24,7 +24,7 @@ namespace RPGHra
 
             int aktualniHP = 0, aktualniMana = 0, aktualniMince = 0, aktualniLevel = 1, aktualniZkusenosti = 0, aktualniUtok = 0;
 
-            int volbaPostavy;
+            int volbaPostavy, vesniceVyber;
             char potvrzeniVyberu;
             bool spravneZadani = false, zmenaPostavy = false, potvrdit = false;
 
@@ -33,6 +33,7 @@ namespace RPGHra
             miniBossDefinice.Add(miniBoss[1], "vládce démonů a černé magie");
 
             Vypisovac vypisovac = new Vypisovac();
+            Vesnice vesnice = new Vesnice();
 
             Console.WriteLine("Vítejte ve hře");
             Console.WriteLine(" __      __                             .__                ");
@@ -91,6 +92,10 @@ namespace RPGHra
             } while (zmenaPostavy == true);
 
             Console.Write("\nNačítání");
+            aktualniHP = postavyHP[volbaPostavy - 1];
+            aktualniMana = mana[volbaPostavy - 1];
+            aktualniMince = mince[volbaPostavy - 1];
+            aktualniUtok = utok[volbaPostavy - 1];
             for (int i = 0; i < 5; i++)
             {
                 Console.Write(".");
@@ -105,13 +110,13 @@ namespace RPGHra
             Console.WriteLine("Jsi poslední nadějí porazit nepřítele.");
             Thread.Sleep(3000);
             Console.WriteLine("Tvá cesta na záchranu vesnice Warspire začíná...");
-            Console.WriteLine("\n[Stiskněte libovolnou klávesu pro pokračování.]");
+            Console.WriteLine("\n[Stiskněte klávesu ENTER pro pokračování.]");
             Console.ReadLine();
-
             Console.Clear();
-            Console.WriteLine("Tvoje cesta začíná v tvé rodné vesnici — Grimharrow.");
+            Console.WriteLine("Tvoje cesta začíná v tvé rodné vesnici — Grimharrow.\n");
+            Console.WriteLine("                     ^  ^  ^       @@@\r\n                   /|\\/|\\/|\\     @@@@@@@\r\n                  /_|__|__|_\\     @@@\r\n                 |  _     _  |     |\r\n                 | | |   | | |    / \\\r\n                 |_|_|___|_|_|   /___\\\r\n                /    (_)    \\    | | |\r\n               /  []     []  \\   |_|_|\r\n              |     ___      |  /=====\\\r\n              |    /___\\     |  |     |\r\n         ___  |______________|__|     |__\r\n        /   \\ |  _______  | |  |     |  \\\r\n       |     || |       | | |  |     |   |\r\n   ~ ~ |  [] || |       | | |  |     |   | ~ ~\r\n  ~ ~ ~|_____|_\\|_______|_|_|__|_____|___| ~ ~");
             Thread.Sleep(4000);
-            Console.WriteLine("Zde jsi momenálně v bezpečí, ale ne nadlouho.");
+            Console.WriteLine("\nZde jsi momenálně v bezpečí, ale ne nadlouho.");
             Thread.Sleep(3000);
             Console.WriteLine("Vesnice jsou místem, kde můžeš vylepšit své schopnosti.");
             Thread.Sleep(3000);
@@ -123,25 +128,123 @@ namespace RPGHra
             Console.ResetColor();
             Console.WriteLine("[2 - Vylepšit maximální životy]");
             Console.WriteLine("[3 - Vylepšit maximální manu]");
-            Console.WriteLine("[4 - Doplnit životy]");
+            Console.WriteLine("[4 - Vylepšit útok]");
             do
                 {
-                while (!int.TryParse(Console.ReadLine(), out volbaPostavy))
+                while (!int.TryParse(Console.ReadLine(), out vesniceVyber))
                     Console.WriteLine("Neplatné zadání, zadejte číslo dle výběru: ");
-                if (volbaPostavy < 1 || volbaPostavy > 4)
+                if (vesniceVyber < 1 || vesniceVyber > 4)
                 {
                     Console.WriteLine("Vyberte pouze čísla postav z nabídky: ");
                     spravneZadani = false;
                 }
-                if (volbaPostavy == 1)
+                if (vesniceVyber == 1)
                 {
                     Console.WriteLine("První volba není dostupná, vyberte jinou:");
+                    spravneZadani = false;
                 }
                 else
                 {
                     spravneZadani = true;
                 }
             } while (spravneZadani == false);
+
+            switch (vesniceVyber)
+            {
+                case 2:
+                    do
+                    {
+                        Console.WriteLine("Vylepšení maximálních životů vás bude stát 20 mincí a bude vylepšeno o 5 HP. Chcete pokračovat? [A/N]");
+                        while (!char.TryParse(Console.ReadLine(), out potvrzeniVyberu))
+                            Console.WriteLine("Neplatné zadání, zadejte číslo dle výběru: ");
+                        if (potvrzeniVyberu == 'A' || potvrzeniVyberu == 'N' || potvrzeniVyberu == 'a' || potvrzeniVyberu == 'n')
+                        {
+                            spravneZadani = true;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Zadejte písmeno A nebo N pro pokračování:");
+                            spravneZadani = false;
+                        }
+                    } while (spravneZadani == false);
+                    if (potvrzeniVyberu == 'N' || potvrzeniVyberu == 'n')
+                    {
+                        break;
+                    }
+                    vesnice.VylepsitZivoty(postavyMaxHP, aktualniMince, volbaPostavy);
+                break;
+                case 3:
+                    do
+                    {
+                        Console.WriteLine("Vylepšení maximální many vás bude stát 30 mincí a bude vylepšena o 5. Chcete pokračovat? [A/N]");
+                        while (!char.TryParse(Console.ReadLine(), out potvrzeniVyberu))
+                            Console.WriteLine("Neplatné zadání, zadejte číslo dle výběru: ");
+                        if (potvrzeniVyberu == 'A' || potvrzeniVyberu == 'N' || potvrzeniVyberu == 'a' || potvrzeniVyberu == 'n')
+                        {
+                            spravneZadani = true;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Zadejte písmeno A nebo N pro pokračování:");
+                            spravneZadani = false;
+                        }
+                    } while (spravneZadani == false);
+                    if (potvrzeniVyberu == 'N' || potvrzeniVyberu == 'n')
+                    {
+                        break;
+                    }
+                    vesnice.VylepsitManu(maxMana, aktualniHP, aktualniMince, volbaPostavy);
+                break;
+                case 4:
+                    do
+                    {
+                        Console.WriteLine("Vylepšení útoku vás bude stát 35 mincí a bude vylepšena o 10 DMG. Chcete pokračovat? [A/N]");
+                        while (!char.TryParse(Console.ReadLine(), out potvrzeniVyberu))
+                            Console.WriteLine("Neplatné zadání, zadejte číslo dle výběru: ");
+                        if (potvrzeniVyberu == 'A' || potvrzeniVyberu == 'N' || potvrzeniVyberu == 'a' || potvrzeniVyberu == 'n')
+                        {
+                            spravneZadani = true;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Zadejte písmeno A nebo N pro pokračování:");
+                            spravneZadani = false;
+                        }
+                    } while (spravneZadani == false);
+                    if (potvrzeniVyberu == 'N' || potvrzeniVyberu == 'n')
+                    {
+                        break;
+                    }
+                    vesnice.VylepsitUtok(utok, aktualniMince, volbaPostavy);
+                break;
+            }
+            
+            /***
+            {
+                case 1:
+                    do
+                    {
+                        Console.WriteLine("Doplnění životů vás bude stát 20 mincí a bude doplněno 10 HP. Chcete pokračovat? [A/N]");
+                        while (!char.TryParse(Console.ReadLine(), out potvrzeniVyberu))
+                            Console.WriteLine("Neplatné zadání, zadejte číslo dle výběru: ");
+                        if (potvrzeniVyberu == 'A' || potvrzeniVyberu == 'N' || potvrzeniVyberu == 'a' || potvrzeniVyberu == 'n')
+                        {
+                            spravneZadani = true;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Zadejte písmeno A nebo N pro pokračování:");
+                            spravneZadani = false;
+                        }
+                    } while (spravneZadani == false);
+                    if (potvrzeniVyberu == 'N' || potvrzeniVyberu == 'n')
+                    {
+                        break;
+                    }
+                    vesnice.DoplnitZivoty(aktualniHP, postavyMaxHP, aktualniMince, volbaPostavy);
+                    break;
+            }
+            ***/
         }
     }
 }
---
+++ RPGHra/Vesnice.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGHra
+{
+    class Vesnice
+    {
+        public void DoplnitZivoty(int aktualniZivoty, int[] maxZivoty, int aktualniMince, int postava)
+        {
+            if (aktualniMince < 20)
+            {
+                Console.WriteLine("Chtěli jste se nechat ošetřit, ale zapomněli jste že nemáte dostatek mincí a místní lékař vás vyhnal z vesnice.");
+                Thread.Sleep(3000);
+                Console.WriteLine("Musíte proto pokračovat dále.");
+                Console.WriteLine("\n[Stiskněte klávesu ENTER pro pokračování.]");
+                Console.ReadLine();
+            } else if (aktualniZivoty == maxZivoty[postava - 1] - 1)
+            {
+                Console.WriteLine("Lékař vás vyšetřil, ale vy ošetření nepotřebujete. Nic jste tedy nemuseli platit.");
+            }
+            else
+            {
+                aktualniMince -= 20;
+                if (aktualniZivoty >= maxZivoty[postava - 1] - 1 && aktualniZivoty <= maxZivoty[postava - 1] - 9)
+                {
+                    aktualniZivoty = maxZivoty[postava - 1];
+                } else
+                {
+                    aktualniZivoty += 10;
+                }
+                Console.WriteLine("Byl jste úspěšně ošetřen a teď jste zdravý jako rybička.");
+                Thread.Sleep(2000);
+                Console.WriteLine("Vaše životy jsou teď " + aktualniZivoty + " HP.");
+                Thread.Sleep(1000);
+                Console.WriteLine("Váš rozpočet je nyní " + aktualniMince + " mincí.");
+                Thread.Sleep(1000);
+            }
+        }
+
+        public void VylepsitZivoty(int[] maxZivoty, int aktualniMince, int postava)
+        {
+            if (aktualniMince < 20)
+            {
+                Console.WriteLine("Vzali jste si posilovací ovoce ale neměli jste čím zaplatit.");
+                Thread.Sleep(2000);
+                Console.WriteLine("Místní farmáři vás chytili, omráčili, ovoce vám sebrali a vyvezli vás z vesnice.");
+                Thread.Sleep(3000);
+                Console.WriteLine("Musíte proto pokračovat dále.");
+                Console.WriteLine("\n[Stiskněte klávesu ENTER pro pokračování.]");
+                Console.ReadLine();
+            }
+            else
+            {
+                aktualniMince -= 20;
+                maxZivoty[postava - 1] += 5;
+                Console.WriteLine("Koupil jste si speciální ovoce z místního sadu, snědl ho a vaše odolnost se zlepšila.");
+                Thread.Sleep(3000);
+                Console.WriteLine("Vaše maximální životy jsou teď " + maxZivoty[postava - 1] + " HP.");
+                Thread.Sleep(1000);
+                Console.WriteLine("Váš rozpočet je nyní " + aktualniMince + " mincí.");
+                Thread.Sleep(1000);
+            }
+        }
+
+        public void VylepsitManu(int[] maxMana, int aktualniHP, int aktualniMince, int postava)
+        {
+            if (aktualniMince < 30)
+            {
+                Console.WriteLine("Čaroděj vám dal lektvar na zlepšení schopností, ale vy jste neměl čím zaplatit.");
+                Thread.Sleep(2000);
+                Console.WriteLine("A tak na vás čaroděj seslal kletbu, účinky lektvaru zneutralizoval, přemístil vás na louku za vesnicí a ubral vám 5 HP.");
+                aktualniHP -= 5;
+                Thread.Sleep(3000);
+                Console.WriteLine("Musíte proto pokračovat dále.");
+                Console.WriteLine("\n[Stiskněte klávesu ENTER pro pokračování.]");
+                Console.ReadLine();
+            }
+            else
+            {
+                aktualniMince -= 30;
+                maxMana[postava - 1] += 5;
+                Console.WriteLine("Místní čaroděj vám dal lektvar a vaše schopnosti se zlepšily.");
+                Thread.Sleep(2000);
+                Console.WriteLine("Vaše maximální mana je teď " + maxMana[postava - 1] + ".");
+                Thread.Sleep(1000);
+                Console.WriteLine("Váš rozpočet je nyní " + aktualniMince + " mincí.");
+                Thread.Sleep(1000);
+            }
+        }
+
+        public void VylepsitUtok(int[] utok, int aktualniMince, int postava)
+        {
+            if (aktualniMince < 35)
+            {
+                Console.WriteLine("Trenér vás chtěl učit ale vy jste neměl čím zaplatit a tak vás vyhnal z města.");
+                Thread.Sleep(2000);
+                Console.WriteLine("Musíte proto pokračovat dále.");
+                Console.WriteLine("\n[Stiskněte klávesu ENTER pro pokračování.]");
+                Console.ReadLine();
+            }
+            else
+            {
+                aktualniMince -= 35;
+                utok[postava - 1] += 10;
+                Console.WriteLine("Zkušený trenér vás úspěšně vycvičil a nyní jste silnější.");
+                Thread.Sleep(2000);
+                Console.WriteLine("Váš útok je nyní " + utok[postava - 1] + " DMG.");
+                Thread.Sleep(1000);
+                Console.WriteLine("Váš rozpočet je nyní " + aktualniMince + " mincí.");
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
